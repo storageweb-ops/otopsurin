17c63dd34a94f548c78721c9e48bff005ec76e24
unknown <guytnpopor@gmail.com>
2025-12-30 14:52:38 +0700

    first commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -95,7 +95,7 @@ const Header = () => {
                 {item.name}
               </Link>
             ))}
-            <div className="pt-4 flex gap-3 hidden">
+            <div className="pt-4 flex gap-3">
               <Button variant="outline" size="sm" className="flex-1">
                 <Search className="h-4 w-4 mr-2" />
                 Search
